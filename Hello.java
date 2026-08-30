a8a1c32b44e384291a621a5dc04a270a04bc4c2a
bmel357 <04724191@nebraska.edu>
2026-08-29 19:00:20 -0500

    initial commit

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/**
+ * Author: Bryan Mellado
+ * Date: 2026/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Bryan Mellado");
+        System.out.println("Robotics Engineering");
+
+    }
+}
